4eff9a05a5adbb4d0ce0eab419c349eccdebbee2
Alejandro Pérez Durán <apduran95@gmail.com>
2025-01-09 00:50:52 -0500

    Update AppSidebar to fetch user data from Supabase and redirect unauthenticated users to sign-in

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,148 +1,149 @@
-"use client"
-
-import {
-  BookOpen,
-  Bot,
-  Frame,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal
-} from "lucide-react"
-import * as React from "react"
-
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
+import { NavUser } from "@/components/nav-user";
 import {
   Sidebar,
-  SidebarContent,
   SidebarFooter,
   SidebarRail
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import * as React from "react";
 
-// This is sample data.
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
-      isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-}
+  // This is sample data.
+// const data = {
+//   user: {
+//     name: "shadcn",
+//     email: "m@example.com",
+//     avatar: "/avatars/shadcn.jpg",
+//   },
+//   navMain: [
+//     {
+//       title: "Playground",
+//       url: "#",
+//       icon: SquareTerminal,
+//       isActive: true,
+//       items: [
+//         {
+//           title: "History",
+//           url: "#",
+//         },
+//         {
+//           title: "Starred",
+//           url: "#",
+//         },
+//         {
+//           title: "Settings",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Models",
+//       url: "#",
+//       icon: Bot,
+//       items: [
+//         {
+//           title: "Genesis",
+//           url: "#",
+//         },
+//         {
+//           title: "Explorer",
+//           url: "#",
+//         },
+//         {
+//           title: "Quantum",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Documentation",
+//       url: "#",
+//       icon: BookOpen,
+//       items: [
+//         {
+//           title: "Introduction",
+//           url: "#",
+//         },
+//         {
+//           title: "Get Started",
+//           url: "#",
+//         },
+//         {
+//           title: "Tutorials",
+//           url: "#",
+//         },
+//         {
+//           title: "Changelog",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Settings",
+//       url: "#",
+//       icon: Settings2,
+//       items: [
+//         {
+//           title: "General",
+//           url: "#",
+//         },
+//         {
+//           title: "Team",
+//           url: "#",
+//         },
+//         {
+//           title: "Billing",
+//           url: "#",
+//         },
+//         {
+//           title: "Limits",
+//           url: "#",
+//         },
+//       ],
+//     },
+//   ],
+//   projects: [
+//     {
+//       name: "Design Engineering",
+//       url: "#",
+//       icon: Frame,
+//     },
+//     {
+//       name: "Sales & Marketing",
+//       url: "#",
+//       icon: PieChart,
+//     },
+//     {
+//       name: "Travel",
+//       url: "#",
+//       icon: Map,
+//     },
+//   ],
+// }
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
+  const supabase = await createClient()
+  
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/sign-in')
+  }
+
+  const user = {
+    name: data?.user?.user_metadata.full_name,
+    email: data?.user?.email || '',
+    avatar: data?.user?.user_metadata.avatar_url,
+  }
+
   return (
     <Sidebar collapsible="icon" {...props}>
-      <SidebarContent>
+      {/* <SidebarContent>
         <NavMain items={data.navMain} />
         <NavProjects projects={data.projects} />
-      </SidebarContent>
+      </SidebarContent> */}
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser user={user} />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
